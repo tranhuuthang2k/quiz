3e4198a64bba6ddf0ae6ebfd10cd44c0401e0247
thangth-2872 <tran.huu.thang@sun-asterisk.com>
2022-04-02 11:37:15 +0700

    update

---
+++ quiz.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+if (isset($_SESSION['user_id']["role"]) == "user") {
+    if ($_SESSION['result']['id'] == $_SESSION['user_id']['id']) {
+        echo ('Bạn đã làm bài rồi');
+    } else {
+
+        $output = '';
+        $output .= '<html">
+    
+       <head>
+           <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    
+           <title>PHP Quiz</title>
+    
+           <link rel="stylesheet" type="text/css" href="css/style.css" />
+       </head>
+    
+       <body>
+           <div id="page-wrap">
+               <h1>Final Quiz for Lip building</h1>
+               <form action="result.php" method="post" id="quiz">
+               <ol>';
+
+        if ($_SESSION['questions']) {
+            foreach ($_SESSION['questions'] as $key) {
+                $output .= '
+                   
+                        <li>
+                        <h3>' . $key['question'] . '</h3> 
+                        <div>
+                            <input type="radio" name="question-' . $key['id'] . '-answers" id="question-' . $key['id'] . '-answers-A" value="' . $key['id'] . '-' . 'A' . '" />
+                            <label for="question-' . $key['id'] . '-answers-A">A) ' . $key['answer_1'] . ' </label>
+                        </div>
+    
+                        <div>
+                            <input type="radio" name="question-' . $key['id'] . '-answers" id="question-' . $key['id'] . '-answers-B" value="' . $key['id'] . '-' . 'B' . '" />
+                            <label for="question-' . $key['id'] . '-answers-B">B) ' . $key['answer_2'] . '</label>
+                        </div>
+    
+                        <div>
+                            <input type="radio" name="question-' . $key['id'] . '-answers" id="question-' . $key['id'] . '-answers-C" value="' . $key['id'] . '-' . 'C' . '" />
+                            <label for="question-' . $key['id'] . '-answers-C">C) ' . $key['answer_3'] . '</label>
+                        </div>
+    
+                        <div>
+                            <input type="radio" name="question-' . $key['id'] . '-answers" id="question-' . $key['id'] . '-answers-D" value="' . $key['id'] . '-' . 'D' . '" />
+                            <label for="question-' . $key['id'] . '-answers-D">D) ' . $key['answer_4'] . '</label>
+                        </div>
+    
+                    </li>
+                    ';
+            }
+        }
+        $output .= ' </ol>
+                   <input type="submit" value="Submit Quiz" />
+               </form>
+           </div>
+       </body>
+    </html>';
+        echo $output;
+    }
+} else {
+    echo 'Bạn ko có quyền truy cập';
+}
---
+++ result.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+    <title>PHP Quiz</title>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+
+    <div id="page-wrap">
+
+        <h1>Final Quiz for Lip building</h1>
+
+        <?php
+        session_start();
+        if ($_SESSION['user_id']) {
+            $answer_1 = $_POST['question-1-answers'];
+            $answer_2 = $_POST['question-2-answers'];
+            $answer = array("question_1" => $answer_1, "question_2" => $answer_2);
+            $id_question_1 = explode("-", $answer_1);
+            $id_question_2 = explode("-", $answer_2);
+            $answer1 = $id_question_1[1];
+            $id_answer_1 = $id_question_1[0];
+            $answer2 = $id_question_2[1];
+            $id_answer_2 = $id_question_2[0];
+            $totalCorrect = 0;
+            if ($answer1 ==  $_SESSION['results'][$id_answer_1]["answer_correct"] ) {
+                $totalCorrect++;
+            }
+            if ($answer2 == $_SESSION['results'][$id_answer_2]["answer_correct"]) {
+                $totalCorrect++;
+            }
+
+            echo "<div id='results'>$totalCorrect / 2 correct</div>";
+            $_SESSION['result'] = array("id" => $_SESSION['user_id']["id"], "answer" =>  $answer, "totalCorrect" =>  $totalCorrect);
+
+            print_r($_SESSION['result']);
+        }
+
+        ?>
+    </div>
+</body>
+</html>
